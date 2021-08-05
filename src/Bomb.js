7a2cb07d6998603903a627f259e5066b22230400
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-08-05 16:38:28 +1000

    Completed Assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,42 @@
-// your Bomb code here!
+/* import React, { Component } from 'react';
+
+export default class Bomb extends React.Component{
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    render(){
+        function message(){
+            if (this.state.secondsLeft >0){ 
+            return(
+                `${this.state.secondsLeft} before I go boom!`)}
+        else{ return("Boom!")}
+
+        }
+        return(
+            <div>{message()}</div>
+        )
+    }
+} */
+
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render(){
+        const message = this.state.secondsLeft ===0? 'Boom!': `${this.state.secondsLeft} seconds left before I go boom!`
+        return (
+            <div>{message}</div>
+        )
+    }
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,21 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+export default class ImageSlider extends Component{
+    constructor(){
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }}
+/*     increment = () => {
+        const newSlide = this.state.currentSlideIndex + 1
+        this.setState({
+            currentSlideIndex = newSlide
+        })
+    } */
+    render(){
+        return(
+            <h1>I am on slide {this.state.currentSlideIndex}</h1>
+        ) }
+        }
+    
